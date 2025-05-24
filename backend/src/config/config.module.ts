5374708e6aba785cf37df7761c3de8d3426c4a41
hellstar-bit <juanluiscamposimanca@gmail.com>
2025-05-23 23:32:31 -0500

    base funcional hasta gestion de usuarios

--- backend/src/config/config.module.ts
+++ backend/src/config/config.module.ts
@@ -1,12 +1,14 @@
-// src/config/config.module.ts
+// backend/src/config/config.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigController } from './config.controller';
 import { Regional } from './entities/regional.entity';
 import { Center } from './entities/center.entity';
 import { Coordination } from './entities/coordination.entity';
 import { Program } from './entities/program.entity';
 import { Ficha } from './entities/ficha.entity';
 import { PersonnelType } from './entities/personnel-type.entity';
+import { Role } from '../users/entities/role.entity';
 
 @Module({
   imports: [
@@ -17,9 +19,10 @@ import { PersonnelType } from './entities/personnel-type.entity';
       Program,
       Ficha,
       PersonnelType,
+      Role, // Agregar Role aquí
     ]),
   ],
-  controllers: [],
+  controllers: [ConfigController], // Agregar el controlador aquí
   providers: [],
   exports: [TypeOrmModule],
 })
